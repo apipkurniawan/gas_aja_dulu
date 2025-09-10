ceb0da585119959f692ca30660d03e083732d117
apipkurniawan <davitaja92@gmail.com>
2025-09-10 21:22:11 +0700

    feat : add image

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,28 @@ const playfair = Playfair_Display({
 export const metadata = {
   title: "Undangan Pernikahan",
   description: "Website Undangan Apip Kurniawan & Nandara Apriliya",
+  openGraph: {
+    title: "Undangan Pernikahan Apip & Nandara Apriliya",
+    description: "Jangan lewatkan momen spesial kami 💍",
+    url: "https://gas-aja-dulu-mbqizfl4u-apipkurniawans-projects.vercel.app",
+    siteName: "Undangan Apip & Nandara Apriliya",
+    images: [
+      {
+        url: "https://yourdomain.com/og-image.jpg", // taruh file di /public
+        width: 1200,
+        height: 630,
+        alt: "Apip & Sinta Wedding",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Undangan Pernikahan Apip & Nandara Apriliya",
+    description: "Jangan lewatkan momen spesial kami 💍",
+    images: ["https://yourdomain.com/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
