8089b3e856bc332d99aa1fba307daecd2d163abb
apipkurniawan <davitaja92@gmail.com>
2025-09-10 21:09:03 +0700

    feat : add desc

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
+import WeddingCard from "./WeddingCard";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex relative items-center justify-center h-screen w-screen">
       <Image
         src="/foto1.jpeg"
         alt="wedding"
-        className="object-cover w-full h-full"
-        width={100}
+        className="object-cover h-full"
+        width={600}
         height={200}
         priority
         unoptimized
       />
+      <div className="absolute inset-0 flex flex-col items-center justify-end pb-12 bg-black/30">
+        {/* name */}
+        <div className="flex gap-24 mb-14">
+          <h1 className="text-[#f5a7b8] drop-shadow-[2px_1px_0px_green] text-2xl font-bold">
+            Nandara Apriliya
+          </h1>
+          <h1 className="text-[#f5a7b8] drop-shadow-[2px_1px_0px_green] text-2xl font-bold">
+            Apip Kurniawan
+          </h1>
+        </div>
+
+        {/* card */}
+        <WeddingCard />
+      </div>
     </div>
   );
 }
